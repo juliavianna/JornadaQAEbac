83684b9d0bb1ad3120d4c2899ea1e6231dac20f0
Julia Vianna <julia.vvianna@gmail.com>
2025-09-29 10:57:35 -0300

    feat: método preencherCadastro

--- testesFrontend/cypress/e2e/spec.cy.js
+++ testesFrontend/cypress/e2e/spec.cy.js
@@ -4,55 +4,31 @@
 describe('US-012: Funcionalidade de cadastro de membros', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
   });
 
   it('cadastro de campos obrigatórios', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#signup-firstname').type('Lucas')
-    cy.get('#signup-lastname').type('AAA')
-    cy.get('#signup-email').type('lucas.silva@example.com')
-    cy.get('#signup-phone').type('1234567890')
-    cy.get('#signup-password').type('senhaForte123!')
-    cy.get('#signup-button').click()
+    var email = `julia${Date.now()}@testes.com`
+    cy.preencherCadastro('Julia', 'Silva', email, '1234567890', 'senhaForte123!')
     cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!')
   })
 
   it('validação de formato de email inválido', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#signup-firstname').type('Teste')
-    cy.get('#signup-lastname').type('Dias')
-    cy.get('#signup-email').type('teste.silva@example')
-    cy.get('#signup-phone').type('1234567890')
-    cy.get('#signup-password').type('senhaForte123!')
-    cy.get('#signup-button').click()
+    cy.preencherCadastro('Teste', 'Dias', 'teste.silva@example', '1234567890', 'senhaForte123!')
     cy.get('#signup-response').should('contain', 'E-mail deve ser um email válido')
   })
 
   it('envio sem preencher campos obrigratórios', () => {
-    cy.visit('http://localhost:3000/')
     cy.get('#signup-button').click()
     cy.get('#signup-response').should('contain', 'Nome não pode estar vazio')
   })
 
   it('validação de bloqueio de senha fraca', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#signup-firstname').type('Ana Julia')
-    cy.get('#signup-lastname').type('Albuquerque')
-    cy.get('#signup-email').type('alo@example.com')
-    cy.get('#signup-phone').type('1234567890')
-    cy.get('#signup-password').type('oi123') // senha fraca
-    cy.get('#signup-button').click()
+    cy.preencherCadastro('Ana Julia', 'Albuquerque', 'alo@example.com', '1234567890', 'oi123')
     cy.get('#signup-response').should('contain', 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula, um número e um caractere especial')
 
   it('cadastro de email duplicado', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#signup-firstname').type('Lucas')
-    cy.get('#signup-lastname').type('Outro')
-    cy.get('#signup-email').type('luscas.silva@example.com')
-    cy.get('#signup-phone').type('1234567890')
-    cy.get('#signup-password').type('senhaForte123!')
-    cy.get('#signup-button').click()
+    cy.preencherCadastro('Lucas', 'Outro', 'luscas.silva@example.com', '1234567890', 'senhaForte123!')
     cy.get('#signup-response').should('contain', 'Este email já está cadastrado')
   })
 })
