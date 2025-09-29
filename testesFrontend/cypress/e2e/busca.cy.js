66efb4e24d3ccd1d5328dea57326d46c8301a4d5
Julia Vianna <julia.vvianna@gmail.com>
2025-09-29 13:34:28 -0300

    refatoração e teste de busca

---
+++ testesFrontend/cypress/e2e/busca.cy.js
@@ -0,0 +1,23 @@
+
+/// <reference types="cypress" />
+
+describe('US-012: Funcionalidade de cadastro de membros', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('buscar filmes com sucesso', () => {
+        cy.get('#search-input').type('Matrix')
+        cy.get('#search-button').click()
+        cy.get('#results-section').should('contain', 'Matrix')
+    })
+
+    it.only('buscar filmes de uma lista com sucesso', () => {
+        cy.fixture('filmes').each((filmes) => {
+            cy.get('#search-input').clear().type(filmes.titulo)
+            cy.get('#search-button').click()
+            cy.get('#results-section').should('contain', filmes.titulo)
+        })
+    });
+});
